9f8a75577f86d5ff29b1191623ba4bd20500e596
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-10-03 04:22:42 +0700

    deleted:    test01 modified:   test01.c

--- rms46/memory/test01.c
+++ rms46/memory/test01.c
@@ -5,6 +5,7 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # INFO: MYADDDRESSES
+ * REV02: Sun 03 Oct 2021 04:22:27 WIB
  * REV01: Sat 02 Oct 2021 18:26:34 WIB
  * START: Sat 03 Apr 2021 06:20:43 WIB
  */
@@ -12,17 +13,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct sysinfo* SYSINFO;
 typedef void* AnyAddrPtr;
 typedef char* ChrPtr;
 typedef char  Chr;
 
+int pcounter=1;
 void printMyAddress (AnyAddrPtr address, ChrPtr message) {
-    printf("[%#16.16X] %s\n", address, message);
+    printf("ZCZC ADDR %2.2d %#16.16X %s\n", pcounter, address, message);
 }
 
 int main(void) {
-    printMyAddress(&main,                    "main");
-    printMyAddress(&printf,                  "printf");
-    printMyAddress(&printMyAddress,          "printMyAddress");
+    printMyAddress(&main,           "&main");
+    printMyAddress(&printf,         "&printf");
+    printMyAddress(&printMyAddress, "&printMyAddress");
+    printMyAddress(&pcounter,       "&pcounter");
 }
 

f501fffafa8ba2bf1b4e7cca784e4ed6256b9697
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-10-09 23:43:20 +0700

    new file:   Makefile new file:   fork new file:   fork1.c new file:   fork2.c

---
+++ rms46/fork/fork2.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2021 Rahmat M. Samik-Ibrahim
+ * http://rahmatm.samik-ibrahim.vlsm.org/
+ * This program is free script/software. This program is distributed in the 
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# INFO: FORK
+ * REV01: Sat 09 Oct 2021 20:28:04 WIB
+ * START: Sat 03 Apr 2021 06:20:00 WIB
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFSIZE1        128
+#define BUFSIZE2        256
+#define BUFSIZE3        1024
+typedef char*           String;
+typedef char            Buf;
+typedef int*            IntPtr;
+typedef unsigned long   UL;
+typedef void*           AnyAddrPtr;
+typedef struct sysinfo  SYSINFO;
+
+#define GETDATE "date +%s"
+Buf    bufEpoch[BUFSIZE1];
+String getEpoch(void) {
+    FILE* filePtr = popen(GETDATE, "r");
+    UL    tmpLong = atol(fgets(bufEpoch, BUFSIZE1, filePtr));
+    pclose (filePtr);
+    sprintf(bufEpoch, "%16.16lX", tmpLong);
+    return (bufEpoch+8);
+}
+
+Buf    bufHostName[BUFSIZE1];
+String getHost(void) {
+    if (gethostname(bufHostName,BUFSIZE1)) strcpy(bufHostName,"HostNameError");
+    return bufHostName;
+}
+
+String getUserName(void) {
+    String userStr=getlogin();
+    if   (userStr==NULL) userStr="UserNameError";
+    return userStr;
+}
+
+#define DOSHASUM   "echo %s|sha1sum|tr '[a-z]' '[A-Z]'| cut -c1-8"
+#define RESULT     8
+Buf    bufSTAMP[BUFSIZE3];
+String getStamp(void) {
+    Buf    tmpSTR[BUFSIZE1];
+    strcpy(tmpSTR,"XXXXXXXX");
+    String tmpEpoch=getEpoch();
+    if (!strcmp(getHost(), getUserName())) {
+        strcpy(tmpSTR,getUserName());
+        strcat(tmpSTR,tmpEpoch);
+        Buf  tmpCMD[BUFSIZE2];
+        sprintf(tmpCMD, DOSHASUM, tmpSTR);
+        FILE* filePtr = popen(tmpCMD, "r");
+        fgets(tmpSTR, RESULT+1, filePtr);
+        tmpSTR[RESULT]=0;
+        pclose(filePtr);
+    }
+    bufSTAMP[0]=0;
+    strcpy(bufSTAMP,tmpEpoch);
+    strcat(bufSTAMP," ");
+    strcat(bufSTAMP,tmpSTR);
+    strcat(bufSTAMP," ");
+    return bufSTAMP;
+}
+
+int pcounter=1;
+void printMyAddress (AnyAddrPtr address, String message) {
+    printf("ZCZC ADDR %2.2d %#16.16lX %s\n", pcounter, (UL) address, message);
+}
+
+void report() {
+    sleep(1);
+    printf("ZCZC PID %5ld", getpid());
+    printf(" PPID %5ld",    getppid());
+    printf(" HOST %s",      getHost());
+    printf(" STAMP %s\n",   getStamp());
+    sleep(1);
+    fflush(NULL);
+}
+
+#define ArraySize   1024*1024
+int main(void) {
+    fork();
+    fork();
+    fork();
+    report();
+    report();
+}
+
